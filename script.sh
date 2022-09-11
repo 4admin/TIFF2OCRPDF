18351063761ce7293e11e732410b21c60761e977
Daniel Neves Dos Santos <56838599+4admin@users.noreply.github.com>
2022-09-11 18:03:59 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
 parallel --tag -j 2 ocrmypdf -l por --output-type pdf '{}' 'output/{}.pdf' ::: *.tif
-# O número 2 diz respeito à quantidade de processos paralelos para agilizar a execução.
+# O número 2 diz respeito à quantidade de processos paralelos para agilizar a execução. No caso do meu notebook, com 3 a minha CPU já batia 100%.
